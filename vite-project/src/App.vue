a49d0a99ef17ed96b3b8fb90734f805eaceaa01e
luca <Luca betti sorbelli>
2023-05-19 15:52:13 +0200

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vite-project/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "AppHeader",
+
+}
+</script>
+
+<template>
+  <header>
+    <h1>ciao</h1>
+  </header>
+</template>
+
+<style scoped>
+/* vale solo per gli header e h1 di questo componente */
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  color: crimson;
+  font-size: 4rem;
+  margin-left: 1rem;
+}
+</style>
+
